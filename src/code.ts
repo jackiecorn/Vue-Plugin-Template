b8af80096b784537bc6270f8243b398a2701d78a
Jackie Chui <jackie2009hk@gmail.com>
2019-06-04 15:32:36 -0700

    Initial commit

---
+++ src/code.ts
@@ -0,0 +1,3 @@
+import { dispatch, handleEvent } from './codeMessageHandler';
+
+figma.showUI(__html__);
---
+++ src/ui.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+div
+</template>
+
+<script>
+import { dispatch, handleEvent } from "./uiMessageHandler";
+export default {
+  data() {
+    return {
+      msg: "Hello world!"
+    };
+  }
+};
+</script>
+
+<style lang='scss'>
+</style>
